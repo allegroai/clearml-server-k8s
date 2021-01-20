793219fed6fe5dabf130cec9da35fe0d168122a4
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2021-01-20 16:22:42 +0100

    ci typo fix

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-CHART_DIR="clearml-server-charts"
+CHART_DIR="clearml-server-chart"
 KUBEVAL_VERSION="0.15.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
